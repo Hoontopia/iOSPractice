7f14b6f92dce5e19da780098271d35b2fab56783
Unknown <sunghun.lim@navercorp.com>
2018-12-02 16:34:39 +0900

    Conform collection view protocols

---
+++ SplitViewPractice/SplitViewPractice/DataCell.swift
@@ -0,0 +1,13 @@
+//
+//  DataCell.swift
+//  SplitViewPractice
+//
+//  Created by 임성훈 on 02/12/2018.
+//  Copyright © 2018 임성훈. All rights reserved.
+//
+
+import UIKit
+
+class DataCell: UICollectionViewCell {
+    
+}
--- SplitViewPractice/SplitViewPractice/ViewController.swift
+++ SplitViewPractice/SplitViewPractice/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let data: [String] = []
+    
+    // MARK: - Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
+// MARK: - UICollectionViewDataSource
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = String(describing: DataCell.self)
+        
+        guard let dataCell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? DataCell else {
+            return DataCell()
+        }
+        
+        return dataCell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UICollectionViewDropDelegate
+
+extension ViewController: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        
+    }
+}
