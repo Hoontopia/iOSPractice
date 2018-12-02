1788f15d1db148595f6d78e2b5f07e44f6925343
Unknown <sunghun.lim@navercorp.com>
2018-12-02 15:56:03 +0900

    Init a split view project

---
+++ SplitViewPractice/SplitViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitViewPractice
+//
+//  Created by 임성훈 on 02/12/2018.
+//  Copyright © 2018 임성훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
